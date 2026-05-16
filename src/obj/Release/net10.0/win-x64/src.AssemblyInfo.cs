71ebe41eabaecdc9447e23a8cc424c8f567bd989
polygonstew <gamedevdata@gmail.com>
2026-05-16 14:36:14 -0400

    -h added

--- src/obj/Release/net10.0/win-x64/src.AssemblyInfo.cs
+++ src/obj/Release/net10.0/win-x64/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6df2120a7cd4a236b2490eaea22aa73843bbe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aeb8f180b508133aabb61843a36cab73ea9c88b")]
 [assembly: System.Reflection.AssemblyProductAttribute("tre")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tre")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
